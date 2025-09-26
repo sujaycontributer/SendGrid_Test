a4b17618918e9f76f83187b77009a1b65d55bd49
Sujay Ghosh <sujayg222@gmail.com>
2025-09-26 22:10:03 +0530

    test the demo mail by sendGrid

---
+++ index.ts
@@ -0,0 +1,20 @@
+import sgMail from "@sendgrid/mail";
+sgMail.setApiKey(process.env.API_KEY_SENDGRID as string);
+
+const msg = {
+  to: "ghoshsujay422@gmail.com",   // recipient
+  from: "sujayg222@gmail.com", // sender (must be verified in SendGrid)
+  subject: "Hello from sujay mail test  SendGrid",
+  text: "This is a plain text email sent using SendGrid + Node.js",
+  html: "<h1>This is an HTML email sent using SendGrid 🎉</h1>",
+};
+
+// Step 3: Send email
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log("Email sent successfully!");
+  })
+  .catch((error) => {
+    console.error("Error sending email:", error);
+});
